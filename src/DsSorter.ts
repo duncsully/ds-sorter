a97e6492692b55a90a968da470e5f921b2a4e951
duncsully <duncsully@gmail.com>
2020-12-12 15:22:29 -0600

    Change random property back to only boolean for now
    Add ability to reverse individual key sort orders
    
    Move mutation observation subscription to updated()
    
    Call customer sorter after getting elements by selector
    
    Redo comparison logic to be a little more robust
    
    Redo value getting logic to be more robust and fix bug with prop strings returning their length

--- src/DsSorter.ts
+++ src/DsSorter.ts
@@ -1,25 +1,21 @@
 import { html, LitElement, property } from 'lit-element';
-// TODO: Allow setting descending per key? (> for descending?) 
 // TODO: Todo list example
 // TODO: Cast attributes? (+ number, ? boolean, : array)
+// TODO: Implement PRNG with optional seed?
+// TODO: Handle comaring different types? 
+const stringToArray = (value: string | null) => value?.split(/,\s*/) ?? []
 export class DsSorter extends LitElement {
 
   /**
-   * If present, sorts randomly. Optionally, assign a non-zero value to use as a seed.
+   * If present, sorts randomly
    */
-  @property({type: Number}) random = NaN
+  @property({type: Boolean}) random = false
 
   /**
-   * A list of values to sort by in order. By default, value is assumed to be an attribute. Changes to any listed attributes on any of the children will automatically cause a re-sort. 
-   * To use a property, prepend "." to the name (e.g. ".checked"). Note that an automatic re-sort won't be triggered for property changes. In those scenarios, it's recommended to listen for events involved in the changing property (e.g. for checked, listen for change event)
+   * A list of keys to sort by in order. By default, key is assumed to be for an attribute. Changes to any listed attributes on any of the children will automatically cause a re-sort. 
+   * To use a property, prepend "." to the key (e.g. ".checked"). Note that an automatic re-sort won't be triggered for property changes. In those scenarios, it's recommended to listen for events involved in the changing property (e.g. for checked, listen for change event)
    */
-  @property({
-    // TODO: would  this be beneficial? reflect: true,
-    converter: {
-      fromAttribute: value => value?.replace(' ', '').split(','),
-      toAttribute: (value: string[]) => value.join(',')
-    }
-  }) by = ['.innerText']
+  @property({converter:  stringToArray}) by = ['.innerText']
 
   /**
    * Custom sort callback
@@ -30,6 +26,11 @@ export class DsSorter extends LitElement {
    * Sort in descending order (else ascending is default)
    */
   @property({type: Boolean}) descending = false
+
+  /**
+   * Any keys to reverse the order of relative to the rest
+   */
+  @property({converter: stringToArray}) reverse: string[] = []
 
   /**
    * Optionally provide a selector with which to grab a descendent of the element being sorted to use the prop/attr/elem of (e.g. ul>ds-sort>li>a>span ':scope > a > span')
@@ -44,9 +45,7 @@ export class DsSorter extends LitElement {
     return Array.from(this.shadowRoot?.querySelector('slot')?.assignedElements() ?? []) as HTMLElement[]
   }
 
-  // TODO: Need to create new one when byAttribute changes?
   mutationObserver = new MutationObserver(mutationList => {
-    console.log(mutationList)
     const shouldUpdate = mutationList.some(mutation => mutation.type === 'attributes' && this.sortingAttributes.length)
     if (shouldUpdate) this.sort()
   })
@@ -58,6 +57,10 @@ export class DsSorter extends LitElement {
 
   updated() {
       this.sort()
+      this._slottedContent.forEach(elem => {
+        const observeElem = (this.selector ? elem.querySelector(this.selector) : elem) as HTMLElement
+        this.mutationObserver.observe(observeElem, { attributes: true, attributeFilter: this.sortingAttributes })
+      })
   }
 
   render() {
@@ -76,19 +79,16 @@ export class DsSorter extends LitElement {
   }
 
   #sorter = (a: HTMLElement, b: HTMLElement): number => {
-    // TODO: Optionally add a seed?
-    if (!isNaN(this.random)) {
+    if (this.random) {
       return Math.random() - 0.5
-    }
-
-    if (this.custom) {
-      return this.custom(a, b)
     }
 
     const firstElem = (this.selector ? a.querySelector(this.selector) : a) as HTMLElement
     const secondElem = (this.selector ? b.querySelector(this.selector) : b) as HTMLElement
 
-    this.mutationObserver.observe(firstElem, { attributes: true, attributeFilter: this.sortingAttributes })
+    if (this.custom) {
+      return this.custom(firstElem, secondElem)
+    }
 
     return this.#compareElements(firstElem, secondElem)
   }
@@ -98,21 +98,48 @@ export class DsSorter extends LitElement {
     const firstVal = this.#getValue(firstElem, key)
     const secondVal = this.#getValue(secondElem, key)
 
-    // If current values are equal, move down the keys until something isn't equal or we run out of keys
-    if (firstVal === secondVal) { return restKeys.length && this.#compareElements(firstElem, secondElem, restKeys) }
-    if (firstVal < secondVal) { return this.descending ? 1 : -1 }
-    return this.descending ? -1 : 1
+    const invertValue = this.descending != this.reverse.includes(key)
+    const [lesser, greater] = invertValue ? [1, -1] : [-1, 1]
+
+
+    if ((firstVal == undefined && secondVal == undefined) || firstVal === secondVal) {
+      // If current values are equal, move down the keys until something isn't equal or we run out of keys
+      return restKeys.length && this.#compareElements(firstElem, secondElem, restKeys)
+    }
+    // defined falsey value is greater than undefined
+    if (firstVal == undefined && secondVal != undefined) {
+      return lesser
+    }
+    if (firstVal != undefined && secondVal == undefined) {
+      return greater
+    }
+    
+    if (firstVal! < secondVal!) {
+      return lesser
+    }
+    return greater
   }
 
   /** Normalize value for comparison */
   #getValue = (elem: HTMLElement, key: string) => {
     // Attributes are always strings
-    if (key[0] !== '.') return elem.getAttribute(key) ?? ''
+    if (key[0] !== '.') return elem.getAttribute(key)
     const propKey = key.substr(1) as keyof HTMLElement
     const prop = elem[propKey]
-    // No nice way to sort by functions, just return that it exists
-    if (typeof prop === 'function') return true
-    // If array-like, return the length, else get the valueOf value (which for strings, numbers, and booleans should just return as is, objects should use the method if it's implemented, else it just gives up)
-    return (prop as ArrayLike<unknown>)?.length ?? prop?.valueOf?.() ?? false
+    const returnAsIs: typeof prop[]  = ['number', 'string', 'boolean', 'bigint', 'undefined']
+    const typeofProp = typeof prop
+    
+    if (returnAsIs.includes(typeofProp)) {
+      return prop
+    }
+    if (typeofProp === 'function') {
+      // No good way to sort functions, just return that it exists
+      return true
+    }
+    if (typeofProp === 'object') {
+      // If array-like, return the length, else get the valueOf value (which for strings, numbers, and booleans should just return as is, objects should use the method if it's implemented)
+      return (prop as ArrayLike<unknown>)?.length ?? prop?.valueOf?.()
+    }
+    return undefined
   }
 }
